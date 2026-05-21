969458f3d7331041b046168da859f874086c8504
qw6740156-pixel <qw6740156@gmail.com>
2026-05-21 11:20:14 +0530

    Study Timer v3.0

--- script.js
+++ script.js
@@ -1,66 +1,194 @@
-let time = 25;
+et time = 25;
+let totalTime = 25;
 let interval;
 let running = false;
+
+let isBreak = false;
 
 let sessions = localStorage.getItem("sessions") || 0;
 
 document.getElementById("timer").innerText = time;
+
 document.getElementById("sessions").innerText =
   "Completed Sessions: " + sessions;
 
+updateProgressBar();
+
 function startTimer() {
+
   if (running) return;
 
-  time = parseInt(document.getElementById("minutes").value) || 25;
+  time =
+    parseInt(document.getElementById("minutes").value) || 25;
+
+  totalTime = time;
 
   document.getElementById("timer").innerText = time;
 
   running = true;
 
   interval = setInterval(() => {
+
     time--;
 
     document.getElementById("timer").innerText = time;
 
-    if (time <= 0) {
-      clearInterval(interval);
+    updateProgressBar();
 
-      alert("Time's up!");
+    if (time <= 0) {
+
+      clearInterval(interval);
 
       running = false;
 
-      sessions++;
+      playSound();
 
-      localStorage.setItem("sessions", sessions);
+      if (!isBreak) {
 
-      document.getElementById("sessions").innerText =
-        "Completed Sessions: " + sessions;
+        sessions++;
 
-      time = 25;
+        localStorage.setItem("sessions", sessions);
+
+        document.getElementById("sessions").innerText =
+          "Completed Sessions: " + sessions;
+
+        startBreak();
+
+      } else {
+
+        startStudy();
+
+      }
     }
+
   }, 1000);
 }
 
 function pauseTimer() {
+
   clearInterval(interval);
 
   running = false;
 }
 
 function resetTimer() {
+
   clearInterval(interval);
 
   running = false;
 
-  time = parseInt(document.getElementById("minutes").value) || 25;
+  isBreak = false;
+
+  document.getElementById("mode").innerText =
+    "Study Mode";
+
+  time =
+    parseInt(document.getElementById("minutes").value) || 25;
+
+  totalTime = time;
 
   document.getElementById("timer").innerText = time;
-}
 
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/studyapp/sw.js");
+  updateProgressBar();
 }
 
 function toggleDarkMode() {
+
   document.body.classList.toggle("dark");
+}
+
+function updateProgressBar() {
+
+  let percent = (time / totalTime) * 100;
+
+  document.getElementById("progress-bar").style.width =
+    percent + "%";
+}
+
+function startBreak() {
+
+  isBreak = true;
+
+  time = 5;
+
+  totalTime = 5;
+
+  document.getElementById("mode").innerText =
+    "Break Mode";
+
+  document.getElementById("timer").innerText = time;
+
+  startAuto();
+}
+
+function startStudy() {
+
+  isBreak = false;
+
+  time =
+    parseInt(document.getElementById("minutes").value) || 25;
+
+  totalTime = time;
+
+  document.getElementById("mode").innerText =
+    "Study Mode";
+
+  document.getElementById("timer").innerText = time;
+
+  startAuto();
+}
+
+function startAuto() {
+
+  running = true;
+
+  interval = setInterval(() => {
+
+    time--;
+
+    document.getElementById("timer").innerText = time;
+
+    updateProgressBar();
+
+    if (time <= 0) {
+
+      clearInterval(interval);
+
+      running = false;
+
+      playSound();
+
+      if (!isBreak) {
+
+        sessions++;
+
+        localStorage.setItem("sessions", sessions);
+
+        document.getElementById("sessions").innerText =
+          "Completed Sessions: " + sessions;
+
+        startBreak();
+
+      } else {
+
+        startStudy();
+
+      }
+    }
+
+  }, 1000);
+}
+
+function playSound() {
+
+  let audio = new Audio(
+    "https://actions.google.com/sounds/v1/alarms/alarm_clock.ogg"
+  );
+
+  audio.play();
+}
+
+if ("serviceWorker" in navigator) {
+
+  navigator.serviceWorker.register("/studyapp/sw.js");
 }
